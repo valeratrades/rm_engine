18eb585d618ac36bf7e229be9a966a6298aaafa3
valeratrades <v79166789533@gmail.com>
2024-12-28 21:49:44 +0000

    -- New Project Snippet --

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = builtins.trace "flake.nix sourced" [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv;
+            packages = [
+              mold-wrapped
+              openssl
+              pkg-config
+              (rust-bin.fromRustupToolchainFile ./.cargo/rust-toolchain.toml)
+            ];
+          };
+      }
+    );
+}
---
+++ src/config.rs
@@ -0,0 +1,20 @@
+use anyhow::Result;
+use std::path::Path;
+use v_utils::macros::MyConfigPrimitives;
+
+#[derive(Clone, Debug, Default, MyConfigPrimitives)]
+pub struct AppConfig {}
+
+impl AppConfig {
+	//TODO!!!: figure out how to return error iff all potentail sources combined fail to provide all of the values;
+	pub fn read(path: &Path) -> Result<Self> {
+		match path.exists() {
+			true => {
+				let builder = config::Config::builder().add_source(config::File::with_name(path.to_str().unwrap()));
+				let raw: config::Config = builder.build()?;
+				Ok(raw.try_deserialize()?)
+			}
+			false => Err(anyhow::anyhow!("Config file does not exist: {:?}", path)),
+		}
+	}
+}
