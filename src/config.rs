62b46357097582c4d5f75ed762cc538e6ea3f90e
valeratrades <v79166789533@gmail.com>
2024-12-29 00:40:00 +0000

    refactor: _

--- src/config.rs
+++ src/config.rs
@@ -8,11 +8,24 @@ pub struct AppConfig {
 
 impl AppConfig {
 	pub fn read() -> Result<Self> {
-		let builder = config::Config::builder()
-			.add_source(config::Environment::default())
-			.add_source(config::File::with_name(&format!("{}/{}", env!("XDG_CONFIG_HOME"), env!("CARGO_PKG_NAME"))).required(true));
+		let app_name = env!("CARGO_PKG_NAME");
+		let locations = [
+			format!("{}/{app_name}", env!("XDG_CONFIG_HOME")),
+			format!("{}/{app_name}/config", env!("XDG_CONFIG_HOME")), //
+		];
 
+		let mut builder = config::Config::builder().add_source(config::Environment::default());
+		for location in locations.iter() {
+			builder = builder.add_source(config::File::with_name(location).required(false));
+		}
 		let raw: config::Config = builder.build()?;
-		Ok(raw.try_deserialize()?)
+
+		match raw.try_deserialize() {
+			Ok(config) => Ok(config),
+			Err(e) => {
+				eprintln!("Config file does not exist or is invalid:");
+				Err(e.into())
+			}
+		}
 	}
 }
