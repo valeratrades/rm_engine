2941d6e673d6c5597c972b2d5ea8923bd884c8a0
valeratrades <v79166789533@gmail.com>
2025-01-06 03:58:55 +0000

    wip

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use clap::{Args, Parser, Subcommand};
 pub mod config;
 use config::AppConfig;
 use v_utils::io::ExpandedPath;
+use v_exchanges::{binance::Binance, core::Exchange};
+use v_exchanges::{bybit::Bybit};
+
 
 #[derive(Parser, Default)]
 #[command(author, version, about, long_about = None)]
@@ -48,5 +51,9 @@ fn start(config: AppConfig, args: StartArgs) {
 }
 
 fn request_total_balance(config: &AppConfig) -> f64 {
-	100.0
+	//HACK: in actuallity get in usdt for now, assume 1:1 with usd
+	let total_binance_usd = 100.0;
+	let total_bybit_usd = 100.0;
+
+	total_binance_usd + total_bybit_usd
 }
