5219595265d77f8c68cf4cac18f78883a847c169
valeratrades <valeratrades@gmail.com>
2025-01-18 21:42:00 +0000

    feat: take proper EMA over previous comp moves

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use chrono::{TimeDelta, Utc};
+use chrono::{DateTime, TimeDelta, Utc};
 use clap::{Args, Parser, Subcommand};
-use color_eyre::eyre::Result;
+use color_eyre::eyre::{Result, bail};
 pub mod config;
 
 use config::AppConfig;
@@ -8,7 +8,7 @@ use v_exchanges::{core::Exchange, prelude::*};
 use v_utils::{
 	Percent,
 	io::ExpandedPath,
-	trades::{Pair, Timeframe},
+	trades::{Kline, Pair, Timeframe},
 };
 
 #[derive(Parser, Default)]
@@ -71,7 +71,7 @@ async fn start(config: AppConfig, args: SizeArgs) -> Result<()> {
 			None => config.default_sl,
 		},
 	};
-	let time = time_since_comp_move(&config, &*bn, &args, price, sl_percent).await?;
+	let time = ema_prev_times_for_same_move(&config, &*bn, &args, price, sl_percent).await?;
 
 	let mul = mul_criterion(time);
 	let target_balance_risk = Percent(*config.default_risk_percent_balance * mul);
@@ -98,26 +98,73 @@ async fn request_total_balance(bn: &dyn Exchange, bb: &dyn Exchange, mx: &dyn Ex
 	*binance_usdc + *binance_usdt + *bybit_usdt + *bybit_usdc + *mexc_usdt
 }
 
-//TODO!: measure 10 back, EMA over them
-async fn time_since_comp_move(_config: &AppConfig, bn: &dyn Exchange, args: &SizeArgs, price: f64, sl_percent: Percent) -> Result<TimeDelta> {
+/// Returns EMA over previous 10 last moves of the same distance.
+async fn ema_prev_times_for_same_move(_config: &AppConfig, bn: &dyn Exchange, args: &SizeArgs, price: f64, sl_percent: Percent) -> Result<TimeDelta> {
+	static RUN_TIMES: usize = 10;
 	let calc_range = |price: f64, sl_percent: Percent| {
 		let sl = price * *sl_percent;
 		(price - sl, price + sl)
 	};
-	let range = calc_range(price, sl_percent);
+	let mut range = calc_range(price, sl_percent);
+	let mut prev_time = Utc::now();
+	let mut times: Vec<TimeDelta> = Vec::default();
 
-	let timeframes: Vec<Timeframe> = vec!["1m".into(), "1h".into(), "1w".into()];
-	for tf in timeframes {
-		let klines = bn.klines(args.pair, tf, 1000.into(), bn.source_market()).await.unwrap();
-		for k in klines.iter().rev() {
-			if k.low < range.0 || k.high > range.1 {
-				return Ok(Utc::now() - k.open_time);
+	let mut check_if_satisfies = |k: &Kline, times: &mut Vec<TimeDelta>, prev_time: &mut DateTime<Utc>| -> bool {
+		let new_anchor = match k {
+			_ if k.low < range.0 => range.0,
+			_ if k.high > range.1 => range.1,
+			_ => return false,
+		};
+		let duration: TimeDelta = *prev_time - k.open_time;
+		*prev_time -= duration;
+		times.push(duration);
+		range = calc_range(new_anchor, sl_percent);
+		true
+	};
+
+	let preset_timeframes: Vec<Timeframe> = vec!["1m".into(), "1h".into(), "1w".into()];
+	let mut approx_correct_tf: Option<Timeframe> = None;
+	for tf in preset_timeframes {
+		if approx_correct_tf.is_none() {
+			let klines = bn.klines(args.pair, tf, 1000.into(), bn.source_market()).await.unwrap();
+			for k in klines.iter().rev() {
+				match check_if_satisfies(k, &mut times, &mut prev_time) {
+					true => {
+						approx_correct_tf = Some(tf);
+						break;
+					}
+					false => continue,
+				}
 			}
 		}
 	}
 
-	//TODO!!!: implement
-	todo!("if sl is at 100% or not all historic data is avaliable, could not have any crosses, shouldn't error (but rn it does, deal with it)");
+	let tf = approx_correct_tf.unwrap();
+	let mut i = 0;
+	while times.len() < RUN_TIMES && i < 10 {
+		let request_range = (prev_time - (tf.duration() * 999), prev_time);
+		let klines = bn.klines(args.pair, tf, request_range.into(), bn.source_market()).await.unwrap();
+		for k in klines.iter().rev() {
+			match check_if_satisfies(k, &mut times, &mut prev_time) {
+				true =>
+					if times.len() == RUN_TIMES {
+						break;
+					},
+				false => continue,
+			}
+		}
+		i += 1;
+	}
+
+	if times.is_empty() {
+		bail!("No data found for the given data & sl, you're on your own.");
+	}
+	// The last is the oldest
+	//let ema = times.iter().fold(0, |acc, x| acc + x.num_seconds()) / times.len() as i64;
+	dbg!(&times);
+	let ema = times.iter().enumerate().fold(0_i64, |acc, (i, x)| (acc + x.num_seconds() * (i as i64 + 1)).try_into().unwrap()) as f64 / ((times.len() + 1) as f64 * times.len() as f64 / 2.0);
+	dbg!(&ema);
+	Ok(TimeDelta::seconds(ema as i64))
 }
 
 fn mul_criterion(time: TimeDelta) -> f64 {
