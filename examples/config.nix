73d3eb19a9a896048ba5a852eec4877ecb251860
Valera <valeratrades@gmail.com>
2025-11-17 21:01:16 +0000

    _

--- examples/config.nix
+++ examples/config.nix
@@ -17,20 +17,20 @@
       binance_tiger = "BINANCE_TIGER_FULL"; #dbg: just testing out to see if this is valid
     in
     [
-      {
-        name = "binance";
-        key.env = "${binance_tiger}_PUBKEY";
-        secret.env = "${binance_tiger}_SECRET";
-      }
-      {
-        name = "bybit";
-        key.env = "QUANTM_BYBIT_SUB_PUBKEY";
-        secret.env = "QUANTM_BYBIT_SUB_SECRET";
-      }
-      {
-        name = "mexc";
-        key.env = "MEXC_READ_PUBKEY";
-        secret.env = "MEXC_READ_SECRET";
-      }
-    ];
+    {
+      name = "binance";
+      key.env = "${binance_tiger}_PUBKEY";
+      secret.env = "${binance_tiger}_SECRET";
+    }
+    {
+      name = "bybit";
+      key.env = "QUANTM_BYBIT_SUB_PUBKEY";
+      secret.env = "QUANTM_BYBIT_SUB_SECRET";
+    }
+    {
+      name = "mexc";
+      key.env = "MEXC_READ_PUBKEY";
+      secret.env = "MEXC_READ_SECRET";
+    }
+  ];
 }
